7ad0e2012a3178e680dcd05eca28a8f26cd2cc93
jayson <jaysonruzario@gmail.com>
2021-05-27 02:35:14 +0530

    37 stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,56 @@ void main() {
 
 //
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppstate();
+  }
+}
+
+class MyAppstate extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerquestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print('answer chosen!');
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    List question = [
+      'whats\'s your favorite color?',
+      'what\'s your favorite animal'
+    ];
+
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text("my first app"),
       ),
-      body: Text("Hello !"),
+      body: Column(
+        children: [
+          Text(question[questionIndex]),
+          ElevatedButton(
+            child: Text('answer 1'),
+            onPressed: () {
+              //anonymous function
+              answerquestion();
+            },
+          ),
+          ElevatedButton(
+            child: Text('answer 2'),
+            onPressed: answerquestion,
+          ),
+          ElevatedButton(
+            child: Text('answer 3'),
+            onPressed: answerquestion,
+          ),
+        ],
+      ),
     ));
   }
 }
